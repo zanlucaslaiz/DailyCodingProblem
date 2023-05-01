fd53e4bd3e46987dd9827ec712f7e74c28372fae
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2023-05-01 16:38:39 -0300

    README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DailyCodingProblem
+
+Repositório feito para salvar os códigos dos desafios diários do site https://www.dailycodingproblem.com/
+
---
+++ dayOne.py
@@ -0,0 +1,8 @@
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+# para resolver preciso de uma lista de numeros.
+# preciso do numero k.
+# preciso somar os numeros da lista ate a soma seja igual ao numero k ou que acabe a lista.
+# se o resultado da soma for igual retorne true e se não retorne false.

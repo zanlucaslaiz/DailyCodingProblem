751500dbffc6e0bc716d4070c033a186c082053d
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2023-05-25 09:17:24 -0300

    fazendo os desafios day_08 e day_16

--- day_08.py
+++ day_08.py
@@ -18,3 +18,20 @@ For example, the following tree has 5 unival subtrees:
   / \
  1   1
 """
+class NodeTree:
+    def __init__(self, key=None, left=None, right=None):
+        self.key = key
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return '%s <- %s -> %s' % (self.left and self.left.key,
+                                    self.key,
+                                    self.right and self.right.key)
+    
+
+
+root = NodeTree(8)
+root.left = NodeTree(2)
+root.right  = NodeTree(8)
+print("Tree: ", root)
--- day_16.py
+++ day_16.py
@@ -3,9 +3,28 @@ EASY
 
 This problem was asked by Twitter.
 
-You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
+You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish 
+this, with the following API:
 
 record(order_id): adds the order_id to the log
 get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
 You should be as efficient with time and space as possible.
 """
+
+class Order():
+    def __init__(self):
+        self.orderId = []
+
+    def record(self, order_id):
+        self.orderId.append(order_id)
+
+    def get_last(self, i):
+        return self.orderId[len(self.orderId) - 1 - i]
+
+if __name__ == "__main__":
+    eCommerceOrder = Order()
+    for i in range(1, 101):
+        eCommerceOrder.record(i)
+    
+    print(eCommerceOrder.get_last(10)) 
+    print(eCommerceOrder.get_last(50))     

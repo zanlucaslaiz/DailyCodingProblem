564775b89f702ad4a07ecb219fc4fb9625afba68
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2023-06-01 10:02:57 -0300

    desafio 9

--- day_09.py
+++ day_09.py
@@ -9,3 +9,19 @@ For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1,
 
 Follow-up: Can you do this in O(N) time and constant space?
 """
+
+def rec(nums, idx):
+    if idx >= len(nums):
+        return 0
+    return max(nums[idx] + rec(nums, idx + 2), rec(nums, idx + 1))
+ 
+def findMaxSum(arr, N):
+    return rec(arr, 0)
+ 
+if __name__ == "__main__":
+    # Creating the array
+    arr = [7, 2, 3, 9, 15, 8]
+    N = len(arr)
+ 
+    # Function call
+    print(findMaxSum(arr, N))

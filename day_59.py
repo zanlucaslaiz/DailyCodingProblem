b6327d8c2b64c2f2162e15352a129e26ac56c41b
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2023-10-23 09:28:00 -0300

    novos desafios

---
+++ day_59.py
@@ -0,0 +1,7 @@
+"""
+HARD
+
+This problem was asked by Google.
+
+Implement a file syncing algorithm for two computers over a low-bandwidth network. What if we know the files in the two computers are mostly the same?
+"""
